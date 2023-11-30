beb985bb8ca0db02aca08c73118b137021d161c9
Dominic Chow <dominic.h.chow@gmail.com>
2023-11-29 22:02:32 -0500

    Add basic search database functionality

--- db.py
+++ db.py
@@ -5,3 +5,36 @@
 
 # https://physionet.org/content/mimiciv/2.2/hosp/patients.csv.gz
 # This should be what's inside the mimic4.db database
+
+import sqlite3
+
+class Mimic_DB:
+    def __init__(self, db_file):
+        self.db_file = db_file
+        self.connection = None
+        self.cursor = None
+        try:
+            self.connection = sqlite3.connect(self.db_file)
+            self.cursor = self.connection.cursor()
+            print(f"Connected to the database: {self.db_file}")
+        except sqlite3.Error as e:
+            print(f"Error connecting to the database: {e}")
+
+    def search(self, table, columns, values):
+        search_str = "SELECT * FROM {} ".format(table)
+        for i in range(len(columns)):
+            if i == 0:
+                search_str += 'WHERE '
+            else:
+                search_str += ' AND '
+            search_str += '{} = ?'.format(columns[i])
+
+        self.cursor.execute(search_str, tuple(values))
+        rows = self.cursor.fetchall()
+        for row in rows:
+            print(row)
+
+
+    def close(self):
+        self.curser.close()
+        self.connection.close()
--- main.py
+++ main.py
@@ -1,9 +1,26 @@
 import sys
+import os
 from admin import *
+from db import *
 
 
 def main():
+
+    if len(sys.argv) != 2:
+        print("Usage: python script.py <file_path>")
+        sys.exit(1)
+
+    db_file = sys.argv[1]
+
+    # Check if the file exists
+    if os.path.exists(db_file):
+        print("File exists!")
+    else:
+        print("File does not exist. Please check the path.")
+
     admin_loggedin = False
+
+    db = Mimic_DB(db_file)
 
     while(1):
         cmd = input('Enter command: ')
@@ -15,12 +32,29 @@ def main():
             else:
                 # Can edit users and stuff here
                 print('')
-        elif cmd == 'Edit':
+        elif cmd == 'edit':
             # Use this to edit a patient in the database
             print('Use this to add/edit/remove patient data')
-        elif cmd == 'View':
+        elif cmd == 'view':
+            filter = ''
+            categories = []
+            values = []
+            print('Enter filters as <Column>=<Value>')
+            print('Type done when finished')
+            while filter != 'done':
+                filter = input('> ')
+                if filter == 'done':
+                    break
+                split_res = filter.split('=')
+                categories.append(split_res[0])
+                values.append(split_res[1])
+
+            print(categories)
+            print(values)
+
             # use this access something from the database
-            print('Use this option to view a patient"s data')
+            db.search('"hosp/patients"', categories, values)
+
         else:
             print('Please etner a valid command')
 
